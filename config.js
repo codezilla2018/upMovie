6cd40ebc1263a9223801812078d5c0f7db6121e8
dasaCoder <dilushadasanayaka@gmail.com>
2018-05-11 21:23:03 +0530

    add config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'your consumer key',
+    consumer_secret: 'your secret',
+    access_token_key: 'your token key',
+    access_token_secret: 'your secret'
+  };
--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 // import expree
 const express = require('express');
 const Twitter = require('twitter');
-//const config  = require('config');
+const config  = require('./config');
 
 // initialize express
 var app = express();
 
 // initialize twitter
-var twitter = new Twitter({
-    consumer_key: 'C8RqMleO8N2ZGSnMAez2RjKQK',
-    consumer_secret: 'YB13RsWagJPJOCidfFJYmQ5jdZy8PNW0zAcjxLo5bZ9GrhS1ee',
-    access_token_key: '830079314318262274-9JrL6Hsz5iQsiOO5Rtcx2nXhlspEzlw',
-    access_token_secret: 'KtNd9wVhrqfB3GECLeGFjgbXxUFQ8TpSRkJUOxRjVv32A'
-  });
+var twitter = new Twitter(config);
 
 
   var status = {
