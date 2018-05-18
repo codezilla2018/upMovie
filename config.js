d1d9820086c31ae962dbfa670bea85bb944c3b12
dasaCoder <dilushadasanayaka@gmail.com>
2018-05-18 10:54:17 +0530

    add axios

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'your consumer key',
-    consumer_secret: 'your secret',
-    access_token_key: 'your token key',
-    access_token_secret: 'your secret'
+    consumer_key: 'KNICv0soH4eTZeXRH8Cvs75A0',
+    consumer_secret: 'IzvL2nMMf7zFyR3gF9NCwlaOueNRomHfIffM9o5Uy5JUuYFZ4d',
+    access_token_key: '830079314318262274-lJTzgUQ63tgN3MK31hCIA8ripbRWiuv',
+    access_token_secret: 'kn9ipTLVTzGvr29A9rpbhFoUFzf0aWe7vtTgJiermmyc3'
   };
--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 // import expree
 const express = require('express');
 const Twitter = require('twitter');
+const https = require('https');
+const axios = require('axios');
 const config  = require('./config');
 
 // initialize express
 var app = express();
+
+let url = 'https://api.themoviedb.org/3/discover/movie?api_key=270ba5c916d80333b03881a53f708cb1&release_date.gte=2018-05-16&release_date.lte=2018-06-17';
+let listOfMovies;
+axios.get(url)
+  .then(response => {
+    listOfMovies = response.data.results;
+    // console.log(response.data.results);
+  })
+
 
 // initialize twitter
 var twitter = new Twitter(config);
 
 
   var status = {
-      status: 'this is my first tweer\nmy name is dilusha'
+      status: 'this is my first tweer\nmy name is dilusha'+ new Date() + '#this'
   }
-  twitter.post('statuses/update',status, function(error, tweet, response) {
-      if(!error) {
-          console.log(tweet);
-      } else {
-          console.log(error);
-      }
-  })
+//   twitter.post('statuses/update',status, function(error, tweet, response) {
+//       if(!error) {
+//           console.log(tweet);
+//       } else {
+//           console.log(error);
+//       }
+//   })
+
+
 
 // twitter.post()
 
