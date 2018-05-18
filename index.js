ee7b61fdaad7e182c3e629fe667a6c4160ddfd8b
dasaCoder <dilushadasanayaka@gmail.com>
2018-05-18 11:58:06 +0530

    add getMovie()

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ const config  = require('./config');
 // initialize express
 var app = express();
 
-let url = 'https://api.themoviedb.org/3/discover/movie?api_key=270ba5c916d80333b03881a53f708cb1&release_date.gte=2018-05-16&release_date.lte=2018-06-17';
+let url = 'https://api.themoviedb.org/3/discover/movie?api_key=270ba5c916d80333b03881a53f708cb1&release_date.gte='+new Date()+1 +'&release_date.lte=2018-06-20';
+console.log(url);
 let listOfMovies;
-axios.get(url)
-  .then(response => {
-    listOfMovies = response.data.results;
-    // console.log(response.data.results);
-  })
+let numOfPages;
+let currentPage;
 
+//getMovies();
 
 // initialize twitter
 var twitter = new Twitter(config);
@@ -37,3 +36,23 @@ var twitter = new Twitter(config);
 // twitter.post()
 
 console.log('statted');
+
+function getRecentReleases(movieList) {
+  for(let x= 0; x<movieList.length; x++) {
+    
+      if(movieList[x]['release_date'] > '2018-05-18') {
+        console.log(movieList[x]['release_date']);
+      }
+
+  }
+}
+
+function getMovies() {
+  axios.get(url)
+    .then(response => {
+      listOfMovies = response.data.results;
+      numOfPages = response.data.total_pages;
+      getRecentReleases(listOfMovies);
+      // console.log(response.data.results);
+    })
+}
