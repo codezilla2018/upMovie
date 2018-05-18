d1a50b970db4702f70c9d36b909350465e5a135c
dasaCoder <dilushadasanayaka@gmail.com>
2018-05-18 12:24:45 +0530

    get upcomming films completed

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const config  = require('./config');
 // initialize express
 var app = express();
 
-let url = 'https://api.themoviedb.org/3/discover/movie?api_key=270ba5c916d80333b03881a53f708cb1&release_date.gte='+new Date()+1 +'&release_date.lte=2018-06-20';
-console.log(url);
+
+
 let listOfMovies;
 let numOfPages;
 let currentPage;
 
-//getMovies();
+
+getMovies();
+
 
 // initialize twitter
 var twitter = new Twitter(config);
@@ -41,18 +43,46 @@ function getRecentReleases(movieList) {
   for(let x= 0; x<movieList.length; x++) {
     
       if(movieList[x]['release_date'] > '2018-05-18') {
-        console.log(movieList[x]['release_date']);
+        console.log(movieList[x]['title']);
       }
 
   }
 }
 
 function getMovies() {
-  axios.get(url)
+  axios.get(genarateUrl(1))
     .then(response => {
       listOfMovies = response.data.results;
       numOfPages = response.data.total_pages;
       getRecentReleases(listOfMovies);
+
+        for(let y = 2; y<=numOfPages; y++) {
+          getMoviesByPage(y)
+        }
       // console.log(response.data.results);
     })
+    .catch(err => {
+      console.log(err.msg);
+    });
+}
+
+function getMoviesByPage(page) {
+  axios.get(genarateUrl(page))
+    .then( response => {
+      listOfMovies = response.data.results;
+      numOfPages = response.data.total_pages;
+      getRecentReleases(listOfMovies);
+    })
+    .catch(err => {
+      console.log(err.msg);
+    })
+}
+
+function genarateUrl(page) {
+  let date = new Date();
+  let minDate = date.getFullYear() + '-' + (date.getMonth()+1) + '-'+ (date.getDate()+1);
+  let maxDate = date.getFullYear() + '-' + (date.getMonth()+1) + '-'+ (date.getDate()+2);
+  let url =  'https://api.themoviedb.org/3/discover/movie?api_key=270ba5c916d80333b03881a53f708cb1&release_date.gte='+ minDate +'&release_date.lte='+ maxDate + '&page=' + page;
+  
+  return url;
 }
