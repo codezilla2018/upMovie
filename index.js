1b976f62b2ad90bc37d8adb369e38db7fe506bbd
dasaCoder <dilushadasanayaka@gmail.com>
2018-05-24 20:46:14 +0530

    change time for heroku

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ console.log('test');
 //   console.log('server start on port '+ port);
 // });
 
-schedule.scheduleJob('40 20 * * *', ()=> { // call get method at 09.03H for every day
+schedule.scheduleJob('17 15 * * *', ()=> { // call get method at 09.03H for every day
   getMovies();
 })
 
@@ -69,7 +69,6 @@ async function getRecentReleases(movieList) {
 
 // get list of movies 
 function getMovies() {
-  console.log('hi');
   axios.get(genarateUrl(1))
     .then(response => {
       listOfMovies = response.data.results;
@@ -138,7 +137,7 @@ function tweet(movie) {
   //making the status
   let title = movie.title.replace(/ /g,"_");
   var status = {
-    status: `${movie.title}\n #${title} #upMovie #new_release #2k18 #codezilla\nhttps://www.youtube.com/watch?v=${movie.trailler}`
+    status: `${movie.title}\n #${title} #upMovie #new_release #2k18 #codezilla2k18\nhttps://www.youtube.com/watch?v=${movie.trailler}`
   }
   //console.log(status);
   
